722a912e0f3d72561db5d1020675e05df0a638e4
tybreakyr <tybreakyr.github@gmail.com>
2016-10-19 06:49:24 -0400

    Yup

--- ShiftShape/Program.cs
+++ ShiftShape/Program.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.CodeDom;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.IO.Pipes;
-using System.Linq;
 using System.Net;
-using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
-using System.Text;
-using System.Threading.Tasks;
+using Devart.Data.PostgreSql;
 
 namespace ShiftShape
 {
@@ -20,44 +17,107 @@ namespace ShiftShape
         {
             var marketInfo = new Marketinfo();
             var requestQueue = new Queue<WebRequest>();
-            requestQueue.Enqueue(WebRequest.Create("https://shapeshift.io/marketinfo/btc_eth"));
+            var endDate = DateTime.Now.AddDays('1');
+            var currentTime = new DateTime();
+            var count = 0;
 
-            var responseQueue = new Queue<WebResponse>();
-            FetchQueueResponses(requestQueue,responseQueue);
+            Console.WriteLine($"Starting Date and Time: {DateTime.Now}");
+            Console.WriteLine($"Ending Date and Time: {endDate}");
+            
+            while (currentTime <= endDate)
+            {
+                requestQueue.Enqueue(WebRequest.Create("https://shapeshift.io/marketinfo/btc_eth"));
+                FetchWebResponses(requestQueue);
+                Console.WriteLine($"Adding request {count++} to Queue");
+                currentTime = DateTime.Now;
+            }
+
+            
+            
+            Console.WriteLine("Press Enter to Continue.");
             Console.ReadLine();
         }
 
-        public static void FetchQueueResponses(Queue<WebRequest> webRequestQueue,Queue<WebResponse> webResponseQueue)
+        //read in web requests from JSON file/DB/webservice
+
+        public static void FetchWebResponses(Queue<WebRequest> webRequestQueue)
         {
-            
-            while (true)
+            var count = 0;
+            while (webRequestQueue.Count > 0)
             {
-                foreach (dynamic webRequest in webRequestQueue)
+                var webRequest = webRequestQueue.Dequeue();
+                using (var response = webRequest.GetResponse())
                 {
-                    webResponseQueue.Enqueue((HttpWebResponse)webRequest.GetResponse());                    
+                    var deserializedResponse = Convert_To_JSON_DeQueue(response);
+                    InsertToDb(ToJson(deserializedResponse));
                 }
+                
+                //Console.WriteLine($"Retrieved Response {count++}");
+                System.Threading.Thread.Sleep(6000);//6 second
             }
         }
 
-        private static dynamic Convert_To_JSON_DeQueue(Queue<WebResponse> webResponseQueue)
+        private static dynamic Convert_To_JSON_DeQueue(WebResponse webResponse)
         {
             //"https://shapeshift.io/marketinfo/btc_eth"
-
-            var response = webResponseQueue.Dequeue();
-
-            try
+            
+            if (webResponse == null) return null;
+            using (var dataStream = webResponse.GetResponseStream())
             {
-                using (var dataStream = response.GetResponseStream())
+                var ser = new DataContractJsonSerializer(typeof(Marketinfo));
+                return dataStream != null ? ser.ReadObject(dataStream) : null;
+            }
+
+        }
+
+        private static PgSqlConnection ConnectToDatabase()
+        {
+            return new PgSqlConnection
+            {
+                Host = "localhost",
+                Port = 5432,
+                Database = "shiftShaper",
+                UserId = "postgres",
+                Password = "postgres"
+            };
+        }
+
+        private static void InsertToDb(string jsonObject)
+        {
+            var connection = ConnectToDatabase();
+            var sqlCommand = new PgSqlCommand
+            {
+                CommandText = $"insert into public.initial_bucket (value) values (\'{jsonObject}\')",
+                Connection = connection
+            };
+            connection.Open();
+            using (connection)
+            {
+                try
                 {
-                    var ser = new DataContractJsonSerializer(typeof(Marketinfo));
-                    return dataStream != null ? ser.ReadObject(dataStream) : null;
+                    var aff = sqlCommand.ExecuteNonQuery();
+
+                    Console.WriteLine(aff + " rows were affected.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(sqlCommand.CommandText);
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Error encountered during INSERT operation.");
                 }
             }
-            finally
-            {
-                //throttled for safety
-                System.Threading.Thread.Sleep(3000);
-            }
+
+        }
+
+        public static string ToJson(dynamic objectToConvert)
+        {
+            var jsonSerializer = new DataContractJsonSerializer(objectToConvert.GetType());
+            var memoryStream = new MemoryStream();
+
+            jsonSerializer.WriteObject(memoryStream, objectToConvert);
+            memoryStream.Position = 0;
+            var streamReader = new StreamReader(memoryStream);
+            return streamReader.ReadToEnd();
         }
 
     }
